89d40684e35a629a030319b154830f32d26ef1c4
jeslapand <126982585+jeslapand@users.noreply.github.com>
2023-08-26 14:06:48 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -16,7 +16,7 @@ const Signup = () => {
     const { name, email, password, cpassword} = credentials;
       
       if(password === cpassword){
-        const response = await fetch("http://localhost:5000/api/auth/createuser", {
+        const response = await fetch("https://backend-liard-two.vercel.app/api/auth/createuser", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
